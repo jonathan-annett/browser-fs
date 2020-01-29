ecd4f7f7c68269564fea322665bd876b85220249
jonathan-annett <jonathan.max.annett@gmail.com>
2020-01-30 07:29:32 +1100

    auto add

---
+++ update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd "$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+../jszip-fs-wrap/update.sh
+cd "$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+./update_git_repos.sh
+node . --build
+git add fs_jszip-browser.* index.js package.json browser-fs.* *.sh 
+git commit -m "auto add"
+git push
+git rev-parse HEAD > ./.git_hash
